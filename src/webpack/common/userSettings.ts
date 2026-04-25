a1caa1ae651eed0c66a7950b7d1f5cca82d6c9fb
Francisco Boni <boboniboni@gmail.com>
2026-04-25 14:45:29 -0300

    Favs

--- src/webpack/common/userSettings.ts
+++ src/webpack/common/userSettings.ts
@@ -4,9 +4,12 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import { findLazy } from "@webpack";
+import { findLazy, find } from "@webpack";
 
 export const UserSettingsActionCreators = {
     FrecencyUserSettingsActionCreators: findLazy(m => m.ProtoClass?.typeName?.endsWith(".FrecencyUserSettings")),
     PreloadedUserSettingsActionCreators: findLazy(m => m.ProtoClass?.typeName?.endsWith(".PreloadedUserSettings")),
+    // FavoriteChannelActionCreators: findLazy(m => m.typeName?.endsWith(".FavoriteChannel")),
+    FavoriteChannelActionCreators: findLazy(m => m.typeName?.endsWith(".FavoriteChannel")),
+    // FavoriteChannelActionCreators: findLazy(m => m.ProtoClass?.typeName?.endsWith(".FavoriteChannel")),
 };
